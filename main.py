d2188982a89db9d8e05e95e6033c83a1c21e63bc
derek-shao <t_boneD@outlook.com>
2025-05-17 14:27:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def main():
         print("9. Exit")
         choice = input("Choose an option: ")
 
+ #hahahahhahahaa
+
         if choice == "1":
             group = input("Enter group name: ")
             name = input("Enter student name: ")
